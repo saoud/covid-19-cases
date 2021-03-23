b57ad2b6833d6ebeca8e70a0db98c0c6b801606a
Saoud <saoud@saoud.dev>
2021-03-22 20:19:32 -0700

    adding basic functionality to app and API calls
    Co-authored-by: Collin Chapman <cchap14@gmail.com>
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CovidCases from './covidCountry';
+// import './CovidCases';
+
+// let API = "https://covid-api.mmediagroup.fr/v1"
+// let cases = `${API}/cases?country=${country}`; 
+// let deaths = `${API}/history?country=${country}&status=deaths`;
+// let vaccine = `${API}/vaccines?country=${country}`;
+
+
+function getElements(response) {
+  if (response.All) {
+    $('.showConfirmed').text(`The number of confirmed cases in ${response.All.country} is ${response.All.confirmed}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function makeApiCall() {
+  const response = await CovidCases.getCases(country);
+  getElements(response);
+  // const jsonifiedResponse = await response.json();
+  // return jsonifiedResponse;
+}
+
+
+
+  $('#countryButton').click(function() {
+    let country = $('#country').val();
+    makeApiCall(country);
+  });
